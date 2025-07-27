83390ca01a75ed8285a25a984097cce3589f9956
trickjonhgamer <trickjonhgamer@gmail.com>
2025-07-27 16:42:39 -0300

    cria a funçao que atualiza a lista

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ const olItens = document.querySelector("#itens");
 const pEntrada = document.querySelector('#entrada');
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
+function atualizaLista() {
+    olItens.innerHTML = "";
+    let lista = getLista();
+    for (let i = 0; i < lista.length; i++) {
+    const li = document.createElement('li');
+    li.textContent = lista[i];
+    olItens.appendChild(li);
+  };
+}
