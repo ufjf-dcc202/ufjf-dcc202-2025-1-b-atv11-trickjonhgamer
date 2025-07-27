fb093d7a752eef67f7526308b357bf7995a31534
trickjonhgamer <trickjonhgamer@gmail.com>
2025-07-27 15:56:12 -0300

    cria referencias para os nosso elementos de interesse no arquivo main.js

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 //main.js
+const olItens = document.querySelector("#itens");
+const pEntrada = document.querySelector('#entrada');
+const btnAdicionar = document.querySelector("#adicionar");
+const btnLimpar = document.querySelector("#limpar");
