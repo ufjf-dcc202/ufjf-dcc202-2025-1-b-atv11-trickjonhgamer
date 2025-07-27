abce978e6745fa59c6e100865e3b609dcc1122ca
trickjonhgamer <trickjonhgamer@gmail.com>
2025-07-27 16:47:08 -0300

    corrige erro em main.js que não deixava a lista aparecer

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //main.js
 import {getLista} from './lista.js';
-const olItens = document.querySelector("#itens");
+const olItens = document.querySelector("#lista");
 const pEntrada = document.querySelector('#entrada');
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
